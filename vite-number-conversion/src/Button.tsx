75152f66d883b7543fd0770cee54a1c3ad425fb3
joaquin telleria <joaquintelleria@gmail.com>
2023-03-12 01:36:44 +0100

    updated css

--- vite-number-conversion/src/Button.tsx
+++ vite-number-conversion/src/Button.tsx
@@ -5,18 +5,8 @@ export interface IButtonProps {
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-const buttonStyle = {
-  background: "#007bff",
-  border: "none",
-  color: "white",
-  padding: "10px 20px",
-  cursor: "pointer",
-  fontSize: "16px",
-  borderRadius: "5px",
-};
-
 const Button: React.FC<IButtonProps> = ({ children, onClick }) => {
-  return <button style={buttonStyle} onClick={onClick}>{children}</button>;
+  return <button onClick={onClick}>{children}</button>;
 };
 
 export default Button;
