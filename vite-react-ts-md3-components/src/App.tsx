94c1f846db7d6e0b4b5e8186a7c244953919241a
Joaquin <joaquintelleria@gmail.com>
2023-03-13 13:27:58 +0100

    elevation

--- vite-react-ts-md3-components/src/App.tsx
+++ vite-react-ts-md3-components/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         iconName={undefined}
         onClick={incrementCounter}
       >
-       <div className="text text-display text-display-small"> Click Me </div>
+       <div className="text text-label text-label-large"> Click Me </div>
       </Button>
       <Button
         configuration="filled"
@@ -26,7 +26,7 @@ function App() {
         iconName={"search"}
         onClick={incrementCounter}
       >
-       <div className="text text-display text-display-small"> Click Me </div>
+       <div className="text text-label text-label-large"> Click Me </div>
       </Button>
       <Button
         configuration="outlined"
@@ -34,7 +34,7 @@ function App() {
         iconName={undefined}
         onClick={incrementCounter}
       >
-       <div className="text text-display text-display-small"> Click Me </div>
+       <div className="text text-label text-label-large"> Click Me </div>
       </Button>
       <Button
         configuration="outlined"
@@ -42,7 +42,7 @@ function App() {
         iconName={"search"}
         onClick={incrementCounter}
       >
-       <div className="text text-display text-display-small"> Click Me </div>
+       <div className="text text-label text-label-large"> Click Me </div>
       </Button>
       <p className="text text-display text-display-large">Counter: {count}</p>
 
@@ -79,6 +79,14 @@ function App() {
         <div className="text text-body text-body-small">Body Large - Roboto 16/24 . +0.5</div>
       </div>
 
+    </div>
+
+    <div style={{display: "flex", flexDirection:"row", alignItems:"flex-start", padding:"50px 60px", gap:"36px"}}>
+      <div className="elevation-light-1" style={{background:"linear-gradient(0deg, rgba(103, 80, 164, 0.05), rgba(103, 80, 164, 0.05))", width:"80px", height:"80px", borderRadius:"16px"}}></div>
+      <div className="elevation-light-2" style={{background:"linear-gradient(0deg, rgba(103, 80, 164, 0.08), rgba(103, 80, 164, 0.08))", width:"80px", height:"80px", borderRadius:"16px"}}></div>
+      <div className="elevation-light-3" style={{background:"linear-gradient(0deg, rgba(113, 90, 174, 0.11), rgba(113, 90, 174, 0.11))", width:"80px", height:"80px", borderRadius:"16px"}}></div>
+      <div className="elevation-light-4" style={{background:"linear-gradient(0deg, rgba(103, 80, 164, 0.12), rgba(103, 80, 164, 0.12))", width:"80px", height:"80px", borderRadius:"16px"}}></div>
+      <div className="elevation-light-5" style={{background:"linear-gradient(0deg, rgba(103, 80, 164, 0.14), rgba(103, 80, 164, 0.14))", width:"80px", height:"80px", borderRadius:"16px"}}></div>
     </div>
 
     </div>
