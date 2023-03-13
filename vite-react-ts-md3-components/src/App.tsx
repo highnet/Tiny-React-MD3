d61b13301aabb79aa58cf9d3a2a20b7ee06d52b7
Joaquin <joaquintelleria@gmail.com>
2023-03-13 11:37:02 +0100

    typography

--- vite-react-ts-md3-components/src/App.tsx
+++ vite-react-ts-md3-components/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         iconName={undefined}
         onClick={incrementCounter}
       >
-        Click Me
+       <div className="text text-display text-display-small"> Click Me </div>
       </Button>
       <Button
         configuration="filled"
@@ -26,7 +26,7 @@ function App() {
         iconName={"search"}
         onClick={incrementCounter}
       >
-        Click Me
+       <div className="text text-display text-display-small"> Click Me </div>
       </Button>
       <Button
         configuration="outlined"
@@ -34,7 +34,7 @@ function App() {
         iconName={undefined}
         onClick={incrementCounter}
       >
-        Click Me
+       <div className="text text-display text-display-small"> Click Me </div>
       </Button>
       <Button
         configuration="outlined"
@@ -42,15 +42,44 @@ function App() {
         iconName={"search"}
         onClick={incrementCounter}
       >
-        Click Me
+       <div className="text text-display text-display-small"> Click Me </div>
       </Button>
-      <p className="text text-display-large">Counter: {count}</p>
+      <p className="text text-display text-display-large">Counter: {count}</p>
 
 
-      <div className="text text-display-large">Display Large - Roboto 57/54 . 0</div>
-      <div className="text text-display-medium">Display Medium - Roboto 45/52 .  0  </div>
-      <div className="text text-display-small">Display Small - Roboto 36/44 . 0  </div>
+    <div style={{display:"flex", flexDirection:"column", alignItems:"flex-start", padding:"70px", gap:"40px"}}>
 
+      <div style={{boxSizing:"border-box", display:"flex", flexDirection:"column", alignItems:"flex-start", padding:"24px", gap: "24px", border:"1px solid #79747E", borderRadius:"16px"}}>
+        <div className="text text-display text-display-large">Display Large - Roboto 57/54 . 0</div>
+        <div className="text text-display text-display-medium">Display Medium - Roboto 45/52 .  0</div>
+        <div className="text text-display text-display-small">Display Small - Roboto 36/44 . 0</div>
+      </div>
+
+      <div style={{boxSizing:"border-box", display:"flex", flexDirection:"column", alignItems:"flex-start", padding:"24px", gap:"24px", border:"1px solid #79747E", borderRadius:"16px"}}>
+        <div className="text text-headline text-headline-large">Headline Large - Roboto 32/40 . 0</div>
+        <div className="text text-headline text-headline-medium">Headline Medium - Roboto 28/36 . 0</div>
+        <div className="text text-headline text-headline-small">Headline Small - Roboto 24/32 . 0</div>
+      </div>
+
+      <div style={{boxSizing:"border-box", display:"flex", flexDirection:"column", alignItems:"flex-start", padding:"24px", gap:"24px", border:"1px solid #79747E", borderRadius:"16px"}}>
+        <div className="text text-title text-title-large">Title Large - Roboto Regular 22/28 . 0</div>
+        <div className="text text-title text-title-medium">Title Medium - Roboto Medium 16/24 . +0.15</div>
+        <div className="text text-title text-title-small">Title Small - Roboto Medium 14/20 . +0.1</div>
+      </div>
+
+      <div style={{boxSizing:"border-box", display:"flex", flexDirection:"column", alignItems:"flex-start", padding:"24px", gap:"24px", border:"1px solid #79747E", borderRadius:"16px"}}>
+        <div className="text text-label text-label-large">Label Large - Roboto Medium 14/20 . +0.1</div>
+        <div className="text text-label text-label-medium">Label Medium - Roboto Medium 12/16 . +0.5</div>
+        <div className="text text-label text-label-small">Label Small - Roboto Medium 11/16 . +0.5</div>
+      </div>
+
+      <div style={{boxSizing:"border-box", display:"flex", flexDirection:"column", alignItems:"flex-start", padding:"24px", gap:"24px", border:"1px solid #79747E", borderRadius:"16px"}}>
+        <div className="text text-body text-body-large">Body Large - Roboto 16/24 . +0.5</div>
+        <div className="text text-body text-body-medium">Body Large - Roboto 16/24 . +0.5</div>
+        <div className="text text-body text-body-small">Body Large - Roboto 16/24 . +0.5</div>
+      </div>
+
+    </div>
 
     </div>
   )
