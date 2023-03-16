cd2ba3d9caecd61868b7d5a3f32889e2081903a4
joaquin telleria <joaquintelleria@gmail.com>
2023-03-16 18:13:39 +0100

    switch class

--- vite-react-ts-md3-components/src/Switch/Switch.tsx
+++ vite-react-ts-md3-components/src/Switch/Switch.tsx
@@ -6,20 +6,20 @@ const Switch: React.FC<ISwitchProps> = ({ disabled, children, id, className, sel
   const [_disabled] = useState(disabled || false);
   const [_id] = useState(id || undefined);
   const [_className] = useState(className || "");
-  
+
   const [_selected, setSelected] = useState(selected || false);
 
   const handleClick = () => {
     setSelected(!_selected);
-    if (onChange){
+    if (onChange) {
       onChange(!_selected);
     }
   };
 
   return (
-    <div id={_id} className={"switch" + " " + _className + " " + "switch-" + (_disabled ? "disabled" : "enabled")}>
-      <div className={"switch-track switch-track-" + (_selected ? "selected" : "deselected") + " " + "switch-track-" + (_disabled ? "disabled" : "enabled")}        
-      onClick={handleClick} >
+    <div id={_id} className={"switch" + " " + "switch-" + (_selected ? "selected" : "deselected") + " " + "switch-" + (_disabled ? "disabled" : "enabled") + " " + _className}>
+      <div className={"switch-track switch-track-" + (_selected ? "selected" : "deselected") + " " + "switch-track-" + (_disabled ? "disabled" : "enabled")}
+        onClick={handleClick} >
         <div className={"switch-handle switch-handle-" + (_selected ? "selected" : "deselected") + " " + "switch-handle-" + (_disabled ? "disabled" : "enabled")}            >
           <div className={"switch-handle-overlay"}>
           </div>
