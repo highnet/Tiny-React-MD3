bf0273cda79ae0316e559cf151acb3306e75e266
Joaquin <joaquintelleria@gmail.com>
2023-03-24 15:43:59 +0100

    cleanup

--- vite-react-ts-md3-components/src/Badge/Frame/badge.tsx
+++ vite-react-ts-md3-components/src/Badge/Frame/badge.tsx
@@ -5,32 +5,34 @@ import Badge from "../Badge";
         <div className="text text-title text-title-large">Badges</div>
         <div className="text text-label text-label-small">Small</div>
         <div style={{backgroundColor:"#F8AF50", width:"50px", height:"50px"}}>
-          <Badge
+        <Badge
             configuration={"small"}
             xOffset={95}
             yOffset={-2}
-            ></Badge>
+            >
+        </Badge>
         </div>
 
         <div className="text text-label text-label-small">Single Digit</div>
         <div style={{backgroundColor:"#F8AF50", width:"100px", height:"100px"}}>
-          <Badge 
+        <Badge 
             configuration={"single-digit"}
             xOffset={93}
             yOffset={-4}
             label={"3"}
-          >
-          </Badge>
+        >
+        </Badge>
         </div>
 
         <div className="text text-label text-label-small">Multiple Digits</div>
         <div style={{backgroundColor:"#F8AF50", width:"150px", height:"150px"}}>
-          <Badge
+        <Badge
             configuration={"multiple-digits"}
             xOffset={93}
             yOffset={-4}
             label={"123"}
-            ></Badge>
+        >
+        </Badge>
         </div>
 
       </div>
