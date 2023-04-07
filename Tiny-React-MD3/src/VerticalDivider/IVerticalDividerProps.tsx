bee10644d1328a743520a38d028657229419e700
Praterstern <62384764+Praterstern@users.noreply.github.com>
2023-04-07 17:05:47 +0200

    Update IVerticalDividerProps.tsx

--- Tiny-React-MD3/src/VerticalDivider/IVerticalDividerProps.tsx
+++ Tiny-React-MD3/src/VerticalDivider/IVerticalDividerProps.tsx
@@ -7,10 +7,11 @@ import { IComponentProps } from "../Component/IComponentProps";
     such as id, className, and style. The interface also defines some specific props for the vertical divider component, such as:
     width: a number value that specifies the width of the divider in pixels
     height: a number value that specifies the height of the divider in pixels
-    inset: a string value that specifies the inset configuration of the divider, such as “top”, “bottom”, “both” or “none”
+    inset: a string value that specifies the inset configuration of the divider, such as “top”, “bottom”, “both”, or “none”
     insetTopHeight: a number value that specifies the height of the top inset in pixels
     insetBottomHeight: a number value that specifies the height of the bottom inset in pixels
     showInsets: a boolean value that indicates whether to show or hide the insets
+    
     The interface is exported so it can be imported and used in other files. 
 */
 
