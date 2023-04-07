0433b651d67b4bd0054b8d56cb5d56f8f4cfc385
Praterstern <62384764+Praterstern@users.noreply.github.com>
2023-04-07 19:01:13 +0200

    Update horizontalDivider.tsx

--- Tiny-React-MD3/src/HorizontalDivider/Frame/horizontalDivider.tsx
+++ Tiny-React-MD3/src/HorizontalDivider/Frame/horizontalDivider.tsx
@@ -3,8 +3,7 @@ import HorizontalDivider from "../HorizontalDivider";
 /* 
     Dividers have an inset prop that determines how much space is left on the left and right sides of the divider. The inset prop can be either
     “none”, “right”, “left”, or “center”. The dividers also have a showInsets prop that controls whether
-    or not to show the inset spaces with a different color. The code demonstrates how to use the HorizontalDivider
-    component with different props and styles.
+    or not to show the inset spaces with a different color.
 */
 
 <div
