ccbb46c1ee722c6350eea60e0be1d733abd94fc0
joaquin telleria <joaquintelleria@gmail.com>
2023-04-12 20:37:37 +0200

    added .prettierrc, which forces everyone's prettier to have indentation tab width of 2

--- Tiny-React-MD3/src/HorizontalDivider/IHorizontalDividerProps.tsx
+++ Tiny-React-MD3/src/HorizontalDivider/IHorizontalDividerProps.tsx
@@ -14,10 +14,10 @@ import { IComponentProps } from "../Component/IComponentProps";
 
 // Define an interface for the divider props
 export interface IHorizontalDividerProps extends IComponentProps {
-	height?: number; // The height of the component
-	width?: number; // The width of the component
-	inset?: string; // The inset configuration of the component
-	insetRightWidth?: number; // the width of the right inset
-	insetLeftWidth?: number; // the width of the left inset
-	showInsets?: boolean; // Wether to show the insets or not (for debugging purposes only)
+  height?: number; // The height of the component
+  width?: number; // The width of the component
+  inset?: string; // The inset configuration of the component
+  insetRightWidth?: number; // the width of the right inset
+  insetLeftWidth?: number; // the width of the left inset
+  showInsets?: boolean; // Wether to show the insets or not (for debugging purposes only)
 }
