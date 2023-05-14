e6c2a488c85c5b97b26c0af6dcb0cc6775d542bd
joaquin telleria <joaquintelleria@gmail.com>
2023-05-14 14:40:46 +0200

    icons are now components

--- src/Icon/Frame/IconsFrame.tsx
+++ src/Icon/Frame/IconsFrame.tsx
@@ -10,51 +10,56 @@
 	Each child element also has a code block displaying the HTML code used to create it
 */
 
+import { StringBuilder } from "../../Gizmos/StringBuilder";
 import { getPreferredScheme } from "../../Gizmos/Themeing";
+import Icon from "../Icon";
 
-function IconsFrame() {
+const IconsFrame: React.FC = () => {
 	const _theme =
 		localStorage.getItem("theme") || getPreferredScheme() + "-theme";
 
+	let _computedFrameClassName = new StringBuilder()
+		.add("flex-trmd3")
+		.add("flex-column-trmd3")
+		.add("frame-trmd3")
+		.add("frame-" + _theme)
+		.add("icons-frame-trmd3")
+		.add("icons-frame-" + _theme + "-trmd3")
+		.toString();
+
 	return (
-		<div
-			className={
-				"flex-trmd3 flex-column-trmd3 frame-trmd3 icons-frame-trmd3 icons-frame-" +
-				_theme +
-				"-trmd3"
-			}
-		>
+		<div className={_computedFrameClassName}>
 			{/* ICONS */}
-			<span className="material-symbols-outlined">hive</span>
+			<Icon iconName="hive"></Icon>
 			<pre>
 				<code className="language-html">
 					{" "}
-					{'<span className="material-symbols-outlined">hive</span>'}{" "}
+					{'<Icon iconName="hive"></Icon>'}{" "}
 				</code>
 			</pre>
-			<span className="material-symbols-outlined">spa</span>
+			<Icon iconName="spa"></Icon>
 			<pre>
 				<code className="language-html">
 					{" "}
-					{'<span className="material-symbols-outlined">spa</span>'}{" "}
+					{'<Icon iconName="spa"></Icon>'}{" "}
 				</code>
 			</pre>
-			<span className="material-symbols-outlined">air</span>
+			<Icon iconName="air"></Icon>{" "}
 			<pre>
 				<code className="language-html">
 					{" "}
-					{'<span className="material-symbols-outlined">air</span>'}{" "}
+					{'<Icon iconName="air"></Icon>'}{" "}
 				</code>
 			</pre>
-			<span className="material-symbols-outlined">water</span>
+			<Icon iconName="water"></Icon>
 			<pre>
 				<code className="language-html">
 					{" "}
-					{'<span className="material-symbols-outlined">water</span>'}{" "}
+					{'<Icon iconName="water"></Icon>'}{" "}
 				</code>
 			</pre>
 		</div>
 	);
-}
+};
 
 export default IconsFrame;
---
+++ src/Icon/IIconProps.tsx
@@ -0,0 +1,5 @@
+import { IComponentProps } from "../Component/IComponentProps";
+
+export interface IIconProps extends IComponentProps {
+	iconName?: string;
+}
