96ae545c281504ea855535cd9a6f59c2a91a99d6
joaquin telleria <joaquintelleria@gmail.com>
2023-05-22 22:02:13 +0200

    terser

---
+++ src/Cards/HorizontalCard/IHorizontalCardProps.tsx
@@ -0,0 +1,5 @@
+import { IComponentProps } from "../../Component/IComponentProps";
+
+export interface IHorizontalCardProps extends IComponentProps {
+    configuration?: string;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteCompression from 'vite-plugin-compression';
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
-    ViteMinifyPlugin({
-      removeAttributeQuotes: true,
-      collapseWhitespace: true,
-      removeComments: true,
-      removeRedundantAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      minifyCSS: true,
-      minifyJS: true,
-      terserOptions: {
-        compress: {
-          drop_console: true,
-          drop_debugger: true,
-        },
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
       },
-    }),
-  ]
+    },
+  },
 });
