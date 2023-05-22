797cf7d0f2797516bf4abcb21441ef85175705f2
joaquin telleria <joaquintelleria@gmail.com>
2023-05-22 23:43:53 +0200

    cards

--- src/Cards/HorizontalCard/IHorizontalCardProps.tsx
+++ src/Cards/HorizontalCard/IHorizontalCardProps.tsx
@@ -2,4 +2,8 @@ import { IComponentProps } from "../../Component/IComponentProps";
 
 export interface IHorizontalCardProps extends IComponentProps {
     configuration?: string;
+    initial?: string;
+    header?: string;
+    subhead?: string;
+    imageSrc?: string;
 }
