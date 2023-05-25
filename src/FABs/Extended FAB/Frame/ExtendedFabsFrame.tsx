6dd1da4f3716fd376ca35403d5c98f123aa40f9e
joaquin telleria <joaquintelleria@gmail.com>
2023-05-25 12:30:15 +0200

    fabs dark

--- src/FABs/Extended FAB/Frame/ExtendedFabsFrame.tsx
+++ src/FABs/Extended FAB/Frame/ExtendedFabsFrame.tsx
@@ -26,26 +26,26 @@ const ExtendedFabsFrame: React.FC = () => {
 
 	return (
 		<div className={_computedFrameClassName}>
-			<Typography variant={"text-label-large"}>Large FABs [Light🚧-Dark🚧]</Typography>
+			<Typography variant={"text-label-large"}>Large FABs</Typography>
 			<ComponentFrameTitle link="https://github.com/highnet/Tiny-React-MD3/blob/master/src/readme.md#%EF%B8%8F-extended-fabs">
-				Extended FABs (Floating Action Buttons) [Light✅-Dark🚧]
+				Extended FABs (Floating Action Buttons)
 			</ComponentFrameTitle>
-			<Typography variant={"text-label-small"}>Surface [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Surface</Typography>
 			<ExtendedFab onClick={undefined}></ExtendedFab>
 			<CodeSnippet>
 				{`<ExtendedFab size="large" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Primary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Primary</Typography>
 			<ExtendedFab configuration="primary" onClick={undefined} />
 			<CodeSnippet>
 				{`<ExtendedFab size="large" configuration="primary" onClick={undefined}></ExtendedFab>`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Secondary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Secondary</Typography>
 			<ExtendedFab configuration="secondary" onClick={undefined} />
 			<CodeSnippet>
 				{`<ExtendedFab size="large" configuration="secondary" onClick={undefined}></ExtendedFab>`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Tertiary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Tertiary</Typography>
 			<ExtendedFab configuration="tertiary" onClick={undefined} />
 			<CodeSnippet>
 				{`<ExtendedFab size="large" configuration="tertiary" onClick={undefined}></ExtendedFab>`}
--- src/FABs/FAB/Frame/FabsFrame.tsx
+++ src/FABs/FAB/Frame/FabsFrame.tsx
@@ -35,67 +35,67 @@ const FabsFrame: React.FC = () => {
 				☁️
 			</Typography>
 			<ComponentFrameTitle link="https://github.com/highnet/Tiny-React-MD3/blob/master/src/readme.md#%EF%B8%8F-fabs">
-				FABs (Floating Action Buttons) [Light✅-Dark🚧]
+				FABs (Floating Action Buttons)
 			</ComponentFrameTitle>
-			<Typography variant={"text-label-large"}>Small FABs [Light✅-Dark🚧]</Typography>
-			<Typography variant={"text-label-small"}>Surface [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-large"}>Small FABs</Typography>
+			<Typography variant={"text-label-small"}>Surface</Typography>
 			<Fab onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Primary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Primary</Typography>
 			<Fab configuration="primary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab configuration="primary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Secondary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Secondary</Typography>
 			<Fab configuration="secondary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab configuration="secondary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Tertiary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Tertiary</Typography>
 			<Fab configuration="tertiary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab configuration="tertiary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-large"}>FABs [Light✅-Dark🚧]</Typography>
-			<Typography variant={"text-label-small"}>Surface [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-large"}>FABs</Typography>
+			<Typography variant={"text-label-small"}>Surface</Typography>
 			<Fab size="medium" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="medium" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Primary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Primary</Typography>
 			<Fab size="medium" configuration="primary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="medium" configuration="primary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Secondary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Secondary</Typography>
 			<Fab size="medium" configuration="secondary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="medium" configuration="secondary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Tertiary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Tertiary</Typography>
 			<Fab size="medium" configuration="tertiary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="medium" configuration="tertiary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-large"}>Large FABs [Light✅-Dark🚧]</Typography>
-			<Typography variant={"text-label-small"}>Surface [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-large"}>Large FABs</Typography>
+			<Typography variant={"text-label-small"}>Surface</Typography>
 			<Fab size="large" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="large" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Primary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Primary</Typography>
 			<Fab size="large" configuration="primary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="large" configuration="primary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Secondary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Secondary</Typography>
 			<Fab size="large" configuration="secondary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="large" configuration="secondary" onClick={undefined} />`}
 			</CodeSnippet>
-			<Typography variant={"text-label-small"}>Tertiary [Light✅-Dark🚧]</Typography>
+			<Typography variant={"text-label-small"}>Tertiary</Typography>
 			<Fab size="large" configuration="tertiary" onClick={undefined} />
 			<CodeSnippet>
 				{`<Fab size="large" configuration="tertiary" onClick={undefined} />`}
