72baedffe395b695b7eeab13f8a80833f3241aee
Joaquin <joaquintelleria@gmail.com>
2023-05-30 13:29:55 +0200

    top app bar

--- src/App Bars/Top App Bar/TopAppBar.tsx
+++ src/App Bars/Top App Bar/TopAppBar.tsx
@@ -6,54 +6,92 @@ import Typography from "../../Typography/Typography";
 import Icon from "../../Icon/Icon";
 
 const TopAppBar: React.FC<ITopAppBarProps> = ({
-	className,
-	id,
-	configuration,
-	title,
-	leadingIcon = {
-		name: "arrow_back",
-		onClick: () => {
-			alert("hi");
-		},
-	},
-	trailingIcons,
+    className,
+    id,
+    configuration,
+    title,
+    leadingIcon = {
+        name: "arrow_back",
+        onClick: () => {
+            alert("hi");
+        },
+    },
+    trailingIcons,
 }) => {
-	const [_className] = useState(className || "");
-	const [_id] = useState(id || undefined);
-	const [_configuration] = useState(configuration || "small");
-	const [_title] = useState(title || "TRMD3");
-	const [_trailingIcons] = useState(
-		trailingIcons || [
-			{ name: "attach_file", onClick: () => {} },
-			{ name: "today", onClick: () => {} },
-			{ name: "more_vert", onClick: () => {} },
-		]
-	);
+    const [_className] = useState(className || "");
+    const [_id] = useState(id || undefined);
+    const [_configuration] = useState(configuration || "small");
+    const [_title] = useState(title || "TRMD3");
+    const [_trailingIcons] = useState(
+        trailingIcons || [
+			{ name: _configuration === "small-centered" ? "account_circle" : "attach_file", onClick: () => { alert("hi"); } },
+            { name: "today", onClick: () => { alert("hi"); } },
+            { name: "more_vert", onClick: () => {alert("hi"); } },
+        ]
+    );
 
-	const _theme =
-		localStorage.getItem("theme") || getPreferredScheme() + "-theme";
+    const _theme =
+        localStorage.getItem("theme") || getPreferredScheme() + "-theme";
 
-	let _computedComponentClassName = new StringBuilder()
-		.add("top-app-bar")
-		.add("top-app-bar-" + _theme)
-		.add(_className)
-		.toString();
+    let _computedComponentClassName = new StringBuilder()
+        .add("top-app-bar")
+        .add("top-app-bar-" + _theme)
+		.add("top-app-bar-" + _configuration)
+        .add(_className)
+        .toString();
 
-	console.log(leadingIcon.name);
+    let iconElements = null;
+    if (_configuration === "small-centered") {
+        iconElements = (
+            <div
+                className={
+                    "icon-container-on-top-app-bar icon-container-on-top-app-bar-" +
+                    _theme
+                }
+                key={0}
+                tabIndex={0}
+            >
+                <Icon
+                    className={"icon-on-top-app-bar icon-on-top-app-bar-" + _theme}
+                    onClick={_trailingIcons[0].onClick}
+                >
+                    {_trailingIcons[0].name}
+                </Icon>
+            </div>
+        );
+    } else {
+        iconElements = _trailingIcons.map((icon, index) => (
+            <div
+                className={
+                    "icon-container-on-top-app-bar icon-container-on-top-app-bar-" +
+                    _theme
+                }
+                key={index}
+                tabIndex={0}
+            >
+                <Icon
+                    className={"icon-on-top-app-bar icon-on-top-app-bar-" + _theme}
+                    onClick={icon.onClick}
+                >
+                    {icon.name}
+                </Icon>
+            </div>
+        ));
+    }
 
-	return (
-		<div id={id} className={_computedComponentClassName}>
-			<div
-				className="icon-container-on-top-app-bar"
-				onClick={leadingIcon.onClick}
-			>
-				<Icon>{leadingIcon.name}</Icon>
-			</div>
-			<Typography variant="text-title-large" className="title-on-top-app-bar">
-				{_title}
-			</Typography>
-		</div>
-	);
+    return (
+        <div id={_id} className={_computedComponentClassName}>
+            <div
+                className="icon-container-on-top-app-bar"
+                onClick={leadingIcon.onClick}
+            >
+                <Icon className="icon-on-top-app-bar">{leadingIcon.name}</Icon>
+            </div>
+            <Typography variant="text-title-large" className="title-on-top-app-bar">
+                {_title}
+            </Typography>
+            {iconElements}
+        </div>
+    );
 };
-
 export default TopAppBar;
--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
 
 	return (
 		<div className={"App"}>
-			<TopAppBar />
+			<TopAppBar configuration="small"/>
 			<NavigationRail
 				className={"main-navigation-rail-trmd3"}
 				fab={{
