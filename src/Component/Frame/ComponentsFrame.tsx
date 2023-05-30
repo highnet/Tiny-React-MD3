c370ac272b832bce6d7febac6b869b458400bf79
Joaquin <joaquintelleria@gmail.com>
2023-05-30 13:35:54 +0200

    top app bars frame

---
+++ src/Component/Frame/ComponentsFrame.tsx
@@ -0,0 +1,46 @@
+import { StringBuilder } from "../../Gizmos/StringBuilder";
+import { getPreferredScheme } from "../../Gizmos/Themeing";
+import CodeSnippet from "../../TRMD3/CodeSnippet/CodeSnippet";
+import ComponentFrameTitle from "../../TRMD3/ComponentFrameTitle/ComponentFrameTitle";
+import Typography from "../../Typography/Typography";
+import Component from "../Component";
+
+
+const ComponentsFrame: React.FC = () => {
+	const _theme =
+		localStorage.getItem("theme") || getPreferredScheme() + "-theme";
+
+	let _computedFrameClassName = new StringBuilder()
+		.add("flex-trmd3")
+		.add("flex-column-trmd3")
+		.add("frame-trmd3")
+		.add("frame-" + _theme + "-trmd3")
+		.add("component-frame-trmd3")
+		.add("component-frame-" + _theme + "-trmd3")
+		.toString();
+
+	let _computedTrmd3SectionTitlesClassName = new StringBuilder()
+		.add("section-title-trmd3")
+		.add("section-title-" + _theme + "-trmd3")
+		.toString();
+
+	return (
+		<div className={_computedFrameClassName}>
+			<Typography
+				id="components-section"
+				variant="text-title-large"
+				className={_computedTrmd3SectionTitlesClassName}
+			>
+				⏬
+			</Typography>
+			<ComponentFrameTitle link="https://www.highnet.xyz">
+				Components
+			</ComponentFrameTitle>
+			<Typography variant={"text-label-small"}>Default</Typography>
+			<Component>Component</Component>
+			<CodeSnippet>{`<Component>Component</Component>`}</CodeSnippet>
+
+		</div>
+	);
+};
+export default ComponentsFrame;
