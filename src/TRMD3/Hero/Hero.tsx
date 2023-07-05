9507a0b82b8029d03242d5b40f10520d491e2b99
joaquin telleria <joaquintelleria@gmail.com>
2023-07-05 20:48:10 +0200

    cleaned up hero

--- src/TRMD3/Hero/Hero.tsx
+++ src/TRMD3/Hero/Hero.tsx
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import { getPreferredScheme } from "../../Gizmos/Themeing";
 import Typography from "../../Typography/Typography";
+import { IHeroProps } from "./IHeroProps";
+import { StringBuilder } from "../../Gizmos/StringBuilder";
 
 const firebaseConfig = {
 	storageBucket: "gs://tiny-react-md3.appspot.com",
@@ -12,13 +14,12 @@ const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const logoRef = ref(storage, "tiny-react-md3-logo.png");
 
-interface IHeroProps {
-	hasLogo?: boolean;
-}
-
 const Hero: React.FC<IHeroProps> = ({ hasLogo }) => {
 	const [_hasLogo] = useState(hasLogo || false);
 	const [logoUrl, setLogoUrl] = useState("");
+
+	const _theme =
+		localStorage.getItem("theme") || getPreferredScheme() + "-theme";
 
 	useEffect(() => {
 		getDownloadURL(logoRef).then((url) => {
@@ -26,63 +27,39 @@ const Hero: React.FC<IHeroProps> = ({ hasLogo }) => {
 		});
 	}, []);
 
-	const _theme =
-		localStorage.getItem("theme") || getPreferredScheme() + "-theme";
-
 	return (
-		<div className={"hero-section-trmd3 hero-section-" + _theme + "-trmd3"}>
+		<div className={"hero-section-trmd3"}>
 			<div className="flex-trmd3">
-				{_hasLogo ? (
+				{_hasLogo && (
 					<img loading="lazy" src={logoUrl} className="logo-trmd3" />
-				) : (
-					""
 				)}
 			</div>
-			<Typography
-				className={"main-title-trmd3 main-title-" + _theme + "-trmd3"}
-				variant="text-display-large"
-			>
+			<Typography className={"main-title-trmd3"} variant="text-display-large">
 				TRMD3
 			</Typography>
 			<Typography
 				variant="text-headline-large"
-				className={"main-subtitle-trmd3 main-subtitle-" + _theme + "-trmd3"}
+				className={"main-subtitle-trmd3"}
 			>
 				Tiny React Material Design 3
 			</Typography>
-			<ul
-				className=" 
-			text text-title text-title-large hero-features-trmd3"
-			>
-				<li>
-					<a
-						className={
-							"hero-link-trmd3 hero-link-text-trmd3 hero-link-text-" +
-							_theme +
-							"-trmd3"
-						}
-						href="https://react.dev/"
-					>
-						React
-					</a>
-					<a
-						className="hero-link-trmd3 hero-handshake-trmd3"
-						href="https://github.com/highnet/Tiny-React-MD3"
-					>
-						🤝
-					</a>
-					<a
-						className={
-							"hero-link-trmd3 hero-link-text-trmd3 hero-link-text-" +
-							_theme +
-							"-trmd3"
-						}
-						href="https://www.figma.com/community/file/1035203688168086460"
-					>
-						MD3
-					</a>
-				</li>
-			</ul>
+			<div className="hero-features-trmd3">
+				<a href="https://react.dev/" className="hero-link-trmd3">
+					<Typography variant="text-headline-medium">React</Typography>
+				</a>
+				<a
+					className="hero-link-trmd3"
+					href="https://github.com/highnet/Tiny-React-MD3"
+				>
+					<Typography variant="text-headline-large">🤝</Typography>
+				</a>
+				<a
+					className={"hero-link-trmd3"}
+					href="https://www.figma.com/community/file/1035203688168086460"
+				>
+					<Typography variant="text-headline-medium">MD3</Typography>
+				</a>
+			</div>
 		</div>
 	);
 };
