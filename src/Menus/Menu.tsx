fc3441c7884da6378d0e27513103fa8928826944
joaquin telleria <joaquintelleria@gmail.com>
2023-07-10 17:45:14 +0200

    cleaned up menu

--- src/Menus/Menu.tsx
+++ src/Menus/Menu.tsx
@@ -4,33 +4,35 @@ import { StringBuilder } from "../Gizmos/StringBuilder";
 import { IMenuProps } from "./IMenuProps";
 
 const Menu: React.FC<IMenuProps> = ({
-    className,
-    id,
-    children,
-    onMouseEnter,
-    onMouseLeave,
-    onMouseMove,
-    onClick,
+	className,
+	id,
+	children,
+	onMouseEnter,
+	onMouseLeave,
+	onMouseMove,
+	onClick,
 }) => {
-    const [_className] = useState(className || "");
-    const [_id] = useState(id || undefined);
+	const [_className] = useState(className || "");
+	const [_id] = useState(id || undefined);
 
-    const _theme =
-        localStorage.getItem("theme") || getPreferredScheme() + "-theme";
+	const menuRef = useRef<HTMLUListElement>(null);
 
-    let _computedComponentClassName = new StringBuilder()
-        .add("menu")
-        .add("menu-" + _theme)
-        .add(_className)
-        .toString();
+	const _theme =
+		localStorage.getItem("theme") || getPreferredScheme() + "-theme";
 
-    const menuRef = useRef<HTMLUListElement>(null);
+	let _computedComponentClassName = new StringBuilder()
+		.add("menu")
+		.add("menu-" + _theme)
+		.add(_className)
+		.toString();
 
-    const handleMouseLeave = () => {
-        if (menuRef.current?.classList.contains("menu-parent")) {
-            menuRef.current.classList.remove("menu-active");
-        }
-    };
+	const handleMouseLeave = () => {
+		/* @ts-ignore */
+		onMouseLeave?.();
+		if (menuRef.current?.classList.contains("menu-parent")) {
+			menuRef.current.classList.remove("menu-active");
+		}
+	};
 
 	useEffect(() => {
 		const handleResize = () => {
@@ -46,19 +48,19 @@ const Menu: React.FC<IMenuProps> = ({
 		};
 	}, []);
 
-    return (
-        <ul
-            id={_id}
-            className={_computedComponentClassName}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            onMouseMove={onMouseMove}
-            onClick={onClick}
-            ref={menuRef}
-        >
-            {children}
-        </ul>
-    );
+	return (
+		<ul
+			id={_id}
+			className={_computedComponentClassName}
+			onMouseEnter={onMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			onMouseMove={onMouseMove}
+			onClick={onClick}
+			ref={menuRef}
+		>
+			{children}
+		</ul>
+	);
 };
 
 export default Menu;
