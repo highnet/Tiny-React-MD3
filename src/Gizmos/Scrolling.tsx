549cad8fbc61b62078ee3fa68997a52e9894004d
joaquin telleria <joaquintelleria@gmail.com>
2023-07-11 18:14:42 +0200

    cleaned up scrolling

--- src/Gizmos/Scrolling.tsx
+++ src/Gizmos/Scrolling.tsx
@@ -1,8 +1,7 @@
 export function scrollToSection(sectionId: string, yOffset = 0) {
 	const section = document.querySelector(`#${sectionId}`);
 	if (section) {
-		const y =
-			section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+		const y = section.getBoundingClientRect().top + window.scrollY + yOffset;
 		window.scrollTo({ top: y, behavior: "smooth" });
 	}
 }
